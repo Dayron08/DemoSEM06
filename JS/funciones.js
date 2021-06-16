46efc3268020782d866df8093f260a44ada163d5
unknown <dayron035@outlook.es>
2021-06-15 21:12:06 -0600

    First commit

---
+++ JS/funciones.js
@@ -0,0 +1,7 @@
+function toggle(){
+    var seccion = document.getElementById("sec")
+    var nav = document.getElementById("navigation")
+    seccion.classList.toggle('active')
+    nav.classList.toggle('active')
+}
+
